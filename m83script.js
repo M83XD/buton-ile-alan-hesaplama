ad1b37ba3c3aa4c779f27ed246c40314f9fac789
M83 <75308710+M83XD@users.noreply.github.com>
2023-03-01 14:45:11 +0200

    Create m83script.js

---
+++ m83script.js
@@ -0,0 +1,9 @@
+function hesapla(sayi){
+const kare = sayi * sayi;
+return kare;
+}
+
+function kareAl(){
+const sonuc = hesapla(3)
+    document.getElementById("sonuc".innerHTML = "işlemin sonucu : + "+ kare)
+}
